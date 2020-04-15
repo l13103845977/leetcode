b846b4280949e08128f50cf99401044c858ed2ca
lsy <13103845977@163.com>
2020-04-15 16:03:13 +0800

    删除无用文件

--- src/main/java/com/leetcode/Test_Comsumer.java
+++ src/main/java/com/leetcode/Test_Comsumer.java
@@ -32,17 +32,4 @@ private  String s="5";
 
 
 
-
-       class ListNode {
-         int val;
-         ListNode next;
-         ListNode(int x) { val = x; }
-     }
-
-    class Solution {
-        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-return null;
-        }
-    }
 }
